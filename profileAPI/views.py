dc4c2ffa2543aad7f68409800ce296b906b9a9d3
Xalil404 <kareemxalil@gmail.com>
2025-06-22 10:39:20 +0100

    Added fix for profile

--- profileAPI/views.py
+++ profileAPI/views.py
@@ -4,33 +4,28 @@ from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 
 # Retrieve or update the logged-in user's profile
 class ProfileDetailUpdateView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser, JSONParser]  # 👈 Add this line
 
     def get_object(self):
-        # This ensures the profile returned is the one related to the logged-in user
         return self.request.user.profile
 
     def update(self, request, *args, **kwargs):
-    # Enable partial updates (PATCH)
         partial = kwargs.pop('partial', True)
         instance = self.get_object()
 
-    # Make the request data mutable
         data = request.data.copy()
 
-    # Handle case for removing profile picture (set to null)
         if 'profile_picture' in data and data['profile_picture'] == 'null':
-            data['profile_picture'] = None  # Set to None to remove the profile picture
+            data['profile_picture'] = None
 
-    # Serialize the data
         serializer = self.get_serializer(instance, data=data, partial=partial)
-
-    # Validate and update
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
 
